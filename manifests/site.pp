5fcb3f65a95c606f41b8d708160838ca266ca895
root <root@ip-172-31-49-91.ec2.internal>
2018-10-09 11:37:45 +0000

    clean up modules

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,5 @@
-node 'ip-172-31-60-30.ec2.internal' 'ip-172-31-63-223.ec2.internal' {
+node default {
+#node 'ip-172-31-60-30.ec2.internal' 'ip-172-31-63-223.ec2.internal' {
 file {'testkumar':
 ensure => file,
 path => '/etc/testkumar',
