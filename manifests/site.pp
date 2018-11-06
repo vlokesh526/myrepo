3f2bb166704e5fc88f42cae663e1831a176ae41b
vl.lokesh@gmail.com <root@ip-172-31-49-91.ec2.internal>
2018-11-06 09:55:55 +0000

    resolving the parsing issues from git changes

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,7 @@
 node default {
-#node 'ip-172-31-60-30.ec2.internal' 'ip-172-31-63-223.ec2.internal' {
-<<<<<<< HEAD
 file {'testgitpuppet':
 ensure => file,
 path => '/etc/testgitpuppet',
 content => 'testing git with puppet along with rocking ravi',
-=======
-file {'testkumar':
-ensure => file,
-path => '/etc/testkumar',
-content => 'testing git with kumar',
->>>>>>> parent of 1b2e0c0... rocking ravi test
 }
 }
