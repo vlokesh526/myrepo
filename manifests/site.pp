1b0eb3c93d641d505b3b918847dd291799c820a1
vl.lokesh@gmail.com <root@ip-172-31-49-91.ec2.internal>
2018-11-06 09:51:33 +0000

    Revert "rocking ravi test"
    This reverts commit 1b2e0c02550f2e95338c50219d303600781d9e63.
    
    Conflicts:
    	manifests/site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,15 @@
 node default {
 #node 'ip-172-31-60-30.ec2.internal' 'ip-172-31-63-223.ec2.internal' {
+<<<<<<< HEAD
 file {'testgitpuppet':
 ensure => file,
 path => '/etc/testgitpuppet',
 content => 'testing git with puppet along with rocking ravi',
+=======
+file {'testkumar':
+ensure => file,
+path => '/etc/testkumar',
+content => 'testing git with kumar',
+>>>>>>> parent of 1b2e0c0... rocking ravi test
 }
 }
