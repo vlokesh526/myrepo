4b20baf31b51b74e957acd51ea18482e30e51d85
vl.lokesh@gmail.com <root@ip-172-31-49-91.ec2.internal>
2018-11-06 09:50:30 +0000

    Revert "dummy commit"
    This reverts commit 4ae1e98d868ac1ef3b6d1a817bce696915686b1a.

--- manifests_bkp_9oct2018/site.pp
+++
@@ -1,8 +0,0 @@
-node default {
-#node 'ip-172-31-60-30.ec2.internal' 'ip-172-31-63-223.ec2.internal' {
-file {'testkumar':
-ensure => file,
-path => '/etc/testkumar',
-content => 'testing git with kumar',
-}
-}
